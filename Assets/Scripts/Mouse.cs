14f5f80757bd68bdbb46cfbee590a3177382966e
Lukas A <93783954+Lu4asz@users.noreply.github.com>
2022-12-03 21:48:38 +0100

    Mouse cursor

---
+++ Assets/Scripts/Mouse.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _cursorImage;
+    void Start()
+    {
+        //sets the base cursor invis
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        _cursorImage.position = Input.mousePosition;
+    }
+}
