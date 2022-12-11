0ae3ca26a9a81cab1eab263c2f5615720b309c98
Emily Sundberg <emilysundberg83@gmail.com>
2022-12-11 12:50:03 +0100

    Fixed instruction RT not disappearing upon start

--- Assets/Scripts/MoveObject.cs
+++ Assets/Scripts/MoveObject.cs
@@ -6,11 +6,20 @@ public class MoveObject : MonoBehaviour
 {
     [SerializeField]
     private Transform _target;
+    [SerializeField]
+    private bool _keepOriginalHeight = true;
 
     public void PerformMove()
     {
         Debug.Log("PerformMove(): pos: " + StartPositionData.position);
-        _target.position = StartPositionData.position;
+        if (_keepOriginalHeight)
+        {
+            Vector3 p = StartPositionData.position;
+            p.y = _target.position.y;
+            _target.position = p;
+        }
+        else { _target.position = StartPositionData.position; }
+        
         _target.rotation = StartPositionData.rotation;
     }
 }
